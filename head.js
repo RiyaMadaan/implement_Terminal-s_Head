23833b39cc49df447c6c78edd5e4f3a051547fa6
Riya <riyamadaan404@gmail.com>
2020-04-11 21:30:40 +0530

    now it will take -n(N) as first agrument and second as file name which will print N no. of lines

--- head.js
+++ head.js
@@ -1,15 +1,24 @@
-let file = process.argv[2];
+const number = process.argv[2];
+const file = process.argv[3];
+const fs = require("fs");
 
-let fs = require("fs");
+let isExists = fs.existsSync(file);
+if(isExists == true){
 
+const N = number.split("-n");
 let content = fs.readFileSync(file, "utf8");
 let line = 1;
 let show = content.split('\n');
-for(line=1;line<=10;line++){
+for(line = 1; line <= N[1]; line++){
   if(show[line] == undefined){
     return;
   }
   else{
   console.log(show[line-1]);
 }
+}
+}
+else{
+  console.log("Error: ENOENT: no such file or directory");
+  return;
 }
